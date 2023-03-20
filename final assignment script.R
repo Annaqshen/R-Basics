a4b010abce121a57bccdd5b1499213ad35f69470
Xiaoqiao Shen <125168519+Annaqshen@users.noreply.github.com>
2023-03-20 01:35:46 -0400

    Add files via upload

---
+++ final assignment script.R
@@ -0,0 +1,27 @@
+
+
+###Final Assignment 
+getwd()
+setwd("/Users/annashen/Desktop/PSC/R")
+
+install.packages("dslabs") 
+library("dslabs")
+#dslads: a lot of database you can use in R. 
+head(murders)
+View(murders)
+
+install.packages("readxl")
+library(readxl)
+mydata<- read_excel("gdp.xls")
+View(mydata)
+
+merge1 <- merge(murders, mydata, by = "state")
+View(merge1)
+
+library(ggplot2)
+plot(mydata$population2010, mydata$GDPPC2010)
+
+plot(mydata$population2010, mydata$GDPPC2020)
+
+plot(mydata$population2010, mydata$`change in Pop`)
+
